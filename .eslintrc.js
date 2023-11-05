5cd6fc7c79f2197c4095e882bd7d80b2268c241a
weikai <weikai999828@gmail.com>
2023-11-05 16:36:56 +0800

    feat: 修改eslint和gitignore

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  plugins: ['prettier'],
-  extends: ['prettier'], // prettier 必须是最后一个，才能确保覆盖
-  rules: {
-    // prettier
-    'prettier/prettier': 'error',
-  },
-}
