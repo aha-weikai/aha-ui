2709bf74d3649d3b3002d306eeaf96845328efab
kaixin <1900461953@qq.com>
2023-11-09 17:40:29 +0800

    chore: 创建icon组件，简单测试bem

---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,12 @@
+<template>
+  <i :class="bem.b()">
+    <slot></slot>
+  </i>
+</template>
+
+<script setup lang="ts">
+import { useNamespace } from '@aha-ui/hooks'
+const bem = useNamespace('icon')
+</script>
+
+<style></style>
