f8a27554463b9b530edc84d022f2a0c7d0f2af17
weikai <weikai999828@gmail.com>
2023-11-12 23:46:43 +0800

    chore: 安装icon

--- packages/components/icon/index.ts
+++ packages/components/icon/index.ts
@@ -1,2 +1,9 @@
 import Icon from './src/icon.vue'
-export default Icon
+import { withInstall } from '@aha-ui/utils'
+
+// 通过withInstall 方法给Icon添加了一个install 方法
+const ElIcon = withInstall(Icon)
+export default ElIcon
+
+// 到处组件的props
+export * from './src/icon'
--- packages/utils/vue/install.ts
+++ packages/utils/vue/install.ts
@@ -2,3 +2,11 @@ import type { Plugin } from 'vue'
 
 // 通过vue提供的plugin 类型和传进来的组件类型T的集合进行确定组件类型具有plugin类型方法，如install方法
 export type SFCWithInstall<T> = T & Plugin
+
+export const withInstall = <T>(comp: T) => {
+  ;(comp as SFCWithInstall<T>).install = function (app) {
+    app.component('ElIcon', comp as SFCWithInstall<T>)
+  }
+
+  return comp as SFCWithInstall<T>
+}
