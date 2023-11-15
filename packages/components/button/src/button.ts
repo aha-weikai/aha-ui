65e4582eac750984438557a76b80339cd871e678
kaixin <1900461953@qq.com>
2023-11-15 17:41:02 +0800

    feat: 增加button组件

---
+++ packages/components/button/src/button.ts
@@ -0,0 +1,60 @@
+import type { ExtractPropTypes, PropType } from 'vue'
+import type Button from './button.vue'
+
+export const buttonEmits = {
+  /**
+   * ## 点击事件
+   * @param evt
+   * @returns
+   */
+  click: (evt: MouseEvent) => evt instanceof MouseEvent,
+}
+
+/**
+ * ## 使用type 来定义按钮的样式
+ */
+export type ButtonType =
+  | ''
+  | 'default'
+  | 'primary'
+  | 'success'
+  | 'warning'
+  | 'info'
+  | 'danger'
+  | 'text'
+
+/**
+ * ## button type 的原始类型
+ */
+export type ButtonNativeType = 'button' | 'submit' | 'reset'
+
+/**
+ * ## button 组件大小
+ */
+export type ButtonSize = '' | 'default' | 'small' | 'large'
+
+export const buttonProps = {
+  size: String as PropType<ButtonSize>,
+  type: {
+    type: String as PropType<ButtonType>,
+  },
+  nativeType: {
+    type: String as PropType<ButtonNativeType>,
+    default: 'button',
+  },
+  disabled: Boolean,
+  autofocus: Boolean,
+  round: Boolean,
+  plain: Boolean,
+  circle: Boolean,
+} as const
+
+/**
+ * ## button props 类型
+ */
+export type ButtonProps = ExtractPropTypes<typeof buttonProps>
+
+/**
+ * ## button 组件实例类型
+ */
+export type ButtonInstance = InstanceType<typeof Button>
---
+++ packages/components/button/src/button.vue
@@ -0,0 +1,54 @@
+<template>
+  <button
+    ref="_ref"
+    :class="[
+      ns.b(),
+      ns.m(type),
+      ns.m(size),
+      ns.is('disabled', disabled),
+      ns.is('plain', plain),
+      ns.is('round', round),
+      ns.is('circle', circle),
+    ]"
+    :disabled="disabled"
+    :autofocus="autofocus"
+    :type="nativeType"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { useNamespace } from '@aha-ui/hooks'
+import { ref } from 'vue'
+import { buttonEmits, buttonProps } from './button'
+
+defineOptions({
+  name: 'ElButton',
+})
+
+defineProps(buttonProps)
+
+const emit = defineEmits(buttonEmits)
+
+/**
+ * ## 按钮 html元素
+ */
+const _ref = ref<HTMLButtonElement>()
+
+const ns = useNamespace('button')
+
+const handleClick = (evt: MouseEvent) => {
+  emit('click', evt)
+}
+
+/**
+ * ## 组件暴露自己的属性以及方法，供外部使用
+ */
+defineExpose({
+  ref: _ref,
+})
+</script>
+
+<style></style>
